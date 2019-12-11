c9507f26643fe2865a83c8d9fd74bc7747ec230f
neydercastano <neydinho7@gmail.com>
2019-12-10 21:21:48 -0500

    Se Agregan Cambios del taller de angular, los 3 primeros puntos en los cuales se pide realizar una lista y mostrarla en html

--- semillero2019/ambiente/semillero-hbt/semillero-padre/semillero-web/src/main/angular/src/app/semillero/componentes/bienvenida-semillero/bienvenida-semillero.ts
+++ semillero2019/ambiente/semillero-hbt/semillero-padre/semillero-web/src/main/angular/src/app/semillero/componentes/bienvenida-semillero/bienvenida-semillero.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SemilleroDTO } from '../../dto/semillero.dto';
 
 
 /**
@@ -12,18 +13,107 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Bienvenidasemillero implements OnInit {
    /**
-     * Atributos que contiene los valores de las variables a luego llamar en "bienvenida-semillero" y se muestra en "bienvenida-component"
+     * Atributo que contiene los valores de las variables a luego llamar en "bienvenida-semillero" y se muestra en "bienvenida-component"
      */
   public nombre_autor : string = 'Neyder Castaño Navarro' ;
+
+   /**
+     * Atributo que contiene los valores de las variables a luego llamar en "bienvenida-semillero" y se muestra en "bienvenida-component"
+     */
   public ciudad : string = 'Montería - Córdoba - Colombia';
-  
+
+ /**
+     * Atributos que contienen los valores de las variables SemilleroDto"
+     */
+  public semilleroDto : SemilleroDTO;
+  public semilleroDto2 : SemilleroDTO;
+  public semilleroDto3 : SemilleroDTO;
+  public semilleroDto4 : SemilleroDTO;
+  public semilleroDto5 : SemilleroDTO;
+
+   /**
+     * Atributo que contiene los valores de las lista Array que se muestra en "bienvenida-component"
+     */
+  public listaDatosComic: Array<SemilleroDTO>;
   
   
   constructor() {
     console.log("entro al constructor del componente bienvenida");
   }
 
+
   ngOnInit(): void {
     
-    }
-}
+
+    this.semilleroDto = new SemilleroDTO();    
+    this.semilleroDto.nombre = "Batman";
+    this.semilleroDto.editorial = "Dc";
+    this.semilleroDto.tematica = "Aventura";
+    this.semilleroDto.numeroPaginas = 100;
+    this.semilleroDto.estado = "INACTIVO";
+    this.semilleroDto.precio = 8000.00;
+    this.semilleroDto.tematica = "AVENTURAS"
+    this.semilleroDto.autores = "Steven J";
+    this.semilleroDto.color = true;
+
+    this.semilleroDto2 = new SemilleroDTO();    
+    this.semilleroDto2.nombre = "Superman";
+    this.semilleroDto2.editorial = "Dc";
+    this.semilleroDto2.tematica = "Animados";
+    this.semilleroDto2.numeroPaginas = 170;
+    this.semilleroDto2.estado = "ACTIVO";
+    this.semilleroDto2.precio = 5000.00;
+    this.semilleroDto2.tematica = "AVENTURAS"
+    this.semilleroDto2.autores = "Alberto V";
+    this.semilleroDto2.color = false;
+
+    
+    this.semilleroDto3 = new SemilleroDTO();    
+    this.semilleroDto3.nombre = "Ironman";
+    this.semilleroDto3.editorial = "Marvel";
+    this.semilleroDto3.tematica = "Animaccion";
+    this.semilleroDto3.numeroPaginas = 90;
+    this.semilleroDto3.estado = "INACTIVO";
+    this.semilleroDto3.precio = 10000.00;
+    this.semilleroDto3.tematica = "Guerra"
+    this.semilleroDto3.autores = "Juan G";
+    this.semilleroDto3.color = true;
+
+    this.semilleroDto4 = new SemilleroDTO();    
+    this.semilleroDto4.nombre = "Mujer Maravilla";
+    this.semilleroDto4.editorial = "DC";
+    this.semilleroDto4.tematica = "Aventura";
+    this.semilleroDto4.numeroPaginas = 120;
+    this.semilleroDto4.estado = "ACTIVO";
+    this.semilleroDto4.precio = 15000.00;
+    this.semilleroDto4.tematica = "AVENTURAS"
+    this.semilleroDto4.autores = "Staan Lee";
+    this.semilleroDto4.color = true;
+
+    this.semilleroDto5 = new SemilleroDTO();    
+    this.semilleroDto5.nombre = "Capitan America";
+    this.semilleroDto5.editorial = "Marvel";
+    this.semilleroDto5.tematica = "Justicia";
+    this.semilleroDto5.numeroPaginas = 160;
+    this.semilleroDto5.estado = "ACTIVO";
+    this.semilleroDto5.precio = 50000.00;
+    this.semilleroDto5.tematica = "AVENTURAS"
+    this.semilleroDto5.autores = "Marvel";
+    this.semilleroDto5.color = true;
+
+    this.listaDatosComic = new Array<SemilleroDTO>();
+   this.listaDatosComic.push(this.semilleroDto);
+   this.listaDatosComic.push(this.semilleroDto2);
+   this.listaDatosComic.push(this.semilleroDto3);
+   this.listaDatosComic.push(this.semilleroDto4);
+   this.listaDatosComic.push(this.semilleroDto5);
+   
+
+   
+  }
+
+
+   
+
+  }
+
---
+++ semillero2019/ambiente/semillero-hbt/semillero-padre/semillero-web/src/main/angular/src/app/semillero/dto/semillero.dto.ts
@@ -0,0 +1,61 @@
+/**
+ * @description Clase semilleroDTO que contiene la informacion de un comic
+ * 
+ * @author Neyder Castaño N 
+ */
+export class SemilleroDTO {
+
+    /**
+    * Indicador de resultado.
+    */
+    public id: number;
+
+    /**
+    * Indicador de resultado.
+    */
+    public nombre: string;
+
+    /**
+    * Indicador de resultado.
+    */
+    public editorial: string;
+
+    /**
+    * Indicador de resultado.
+    */
+    public tematica: string;
+
+    /**
+    * Indicador de resultado.
+    */
+    public numeroPaginas: number;
+
+    /**
+    * Indicador de resultado.
+    */
+    public precio: number;
+
+    /**
+    * Indicador de resultado.
+    */
+    public autores: string;
+
+    /**
+    * Indicador de resultado.
+    */
+    public color: boolean;
+
+    /**
+    * Indicador de resultado.
+    */
+    public fechaVenta: Date;
+
+    /**
+    * Indicador de resultado.
+    */
+    public estado: string;
+
+    
+
+  
+}
